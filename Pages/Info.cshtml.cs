76464d99201d6a7c0c1f9d3c35d43bbf9e61ffa4
DanielCouperthwaite <danielcoup@hotmail.co.uk>
2023-09-15 19:02:48 +0100

    fix asp route name in stock page

--- Pages/Info.cshtml.cs
+++ Pages/Info.cshtml.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -7,14 +11,25 @@ namespace MyApp.Namespace
 {
     public class InfoModel : PageModel
     {
-        // public List<Guitar> Guitars = Inventory.ToList();
 
-        // public Guitar CurrentGuitar { get; set; }
+        public List<Guitar> Guitars = Inventory.ToList();
 
-        public void OnGet(string name)
+
+        public Guitar? CurrentGuitar { get; set; }
+
+        
+        public IActionResult OnGet(string name)
         {
-            // CurrentGuitar = Guitars.Find(guitar => guitar.Name.ToLower() == name.ToLower());
+            CurrentGuitar = Guitars.Find(guitar => guitar.Name.ToLower() == name.ToLower());
 
+            if(CurrentGuitar == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                return Page();
+            }
         }
     }
 }
