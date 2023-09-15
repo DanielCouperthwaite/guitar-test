64c020796afb94b10650958c88639afb22e32063
DanielCouperthwaite <danielcoup@hotmail.co.uk>
2023-09-15 23:50:44 +0100

    add back navigation to info page

--- Models/Guitar.cs
+++ Models/Guitar.cs
@@ -4,17 +4,11 @@ namespace guitar_shop.Models
   public class Guitar
   {
     public string Name { get; set; }
-
     public string ImageSrc { get; set; }
-
     public string Location { get; set; }
-
     public double Price { get; set; }
-
     public bool Available { get; set; }
-
     public string Description { get; set; }
-
     public Guitar(string name = "Not Found", string imageSrc = "/images/blank.png", string location = "Not Found", double price = 0.0, bool available = false, string description = "n/a")
     {
       Name = name;
