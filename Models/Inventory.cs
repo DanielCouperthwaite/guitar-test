216f20ee10774104267e8ea30b24a15b5ed0c43f
DanielCouperthwaite <danielcoup@hotmail.co.uk>
2023-09-15 19:17:33 +0100

    alter guitar information

--- Models/Inventory.cs
+++ Models/Inventory.cs
@@ -12,8 +12,8 @@ namespace guitar_shop.Models
     {
         return new List<Guitar>
         {
-            new Guitar(name: "Gibson", imageSrc: "https://cdn11.bigcommerce.com/s-hvzya2q3vy/images/stencil/1920w/products/49527/136044/bstgbssi01548265__76984.1694534550.jpg?c=1", location: "Preston", price: 399.99, available: true, description: "Brand new! Never used!"),
-            new Guitar(name: "Fender", imageSrc: "https://cdn11.bigcommerce.com/s-hvzya2q3vy/images/stencil/1280x1280/products/49071/133848/bst2usfenams60thannobm__69240.1690905010.jpg?c=1&imbypass=on", location: "Liverpool", price: 449.99, available: false, description: "Some wear and tear"),
+            new Guitar(name: "Gibson Les Paul", imageSrc: "https://cdn11.bigcommerce.com/s-hvzya2q3vy/images/stencil/1920w/products/49527/136044/bstgbssi01548265__76984.1694534550.jpg?c=1", location: "Preston", price: 399.99, available: true, description: "Brand new! Never used!"),
+            new Guitar(name: "Fender Stratocaster", imageSrc: "https://cdn11.bigcommerce.com/s-hvzya2q3vy/images/stencil/1280x1280/products/49071/133848/bst2usfenams60thannobm__69240.1690905010.jpg?c=1&imbypass=on", location: "Liverpool", price: 449.99, available: false, description: "Some wear and tear"),
         };
     }
   }
