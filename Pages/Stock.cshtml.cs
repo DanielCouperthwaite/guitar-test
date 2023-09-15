dadbbd97ac85ebf6914b3cff287e6895f7fcb458
DanielCouperthwaite <danielcoup@hotmail.co.uk>
2023-09-15 13:53:39 +0100

    add stock page to navigation

---
+++ Pages/Stock.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class StockModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
